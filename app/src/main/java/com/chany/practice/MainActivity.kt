0309f77e5b5d905c32370fcdc4b03c074dc7310f
chanydong <chanydong@kaonmedia.com>
2024-02-23 17:34:02 +0900

    Jetpack test

--- app/src/main/java/com/chany/practice/MainActivity.kt
+++ app/src/main/java/com/chany/practice/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.chany.practice
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chany.practice.ui.theme.PracticeTheme
+import com.google.android.material.slider.Slider
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             PracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -43,4 +55,38 @@ fun GreetingPreview() {
     PracticeTheme {
         Greeting("Android")
     }
+}
+
+@Preview
+@Composable
+fun DemoTestView() {
+    DemoTest(message = "DemoTestView 2", fontSize = 12f)
+}
+
+@Composable
+fun DemoTest(message: String, fontSize: Float) {
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, positionChange: (Float) -> Unit){
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..40f,
+        value = sliderPosition,
+        onValueChange = {
+            positionChange(it)
+        }
+    )
+}
+
+@Composable
+fun DemoScreen() {
+
+    var sliderPosition by remember { mutableStateOf(20f) }
+
 }
--- app/src/main/java/com/chany/practice/SubActivity.kt
+++ app/src/main/java/com/chany/practice/SubActivity.kt
@@ -8,4 +8,5 @@ class SubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
     }
+
 }
