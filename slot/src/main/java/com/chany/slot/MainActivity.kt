453db26f92a6043a15f02878cc0a349bda6e24ab
chanydong <chanydong@kaonmedia.com>
2024-03-06 17:56:38 +0900

    practice(220p)

--- slot/src/main/java/com/chany/slot/MainActivity.kt
+++ slot/src/main/java/com/chany/slot/MainActivity.kt
@@ -3,13 +3,26 @@ package com.chany.slot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chany.slot.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
+
                 }
             }
         }
@@ -30,17 +44,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+
+    var linearSelected by remember { mutableStateOf(true) }
+    var imageSelected by remember { mutableStateOf(true) }
+
+    val onLinearClick = { value : Boolean -> linearSelected = value }
+    val onTitleClick = { value : Boolean -> imageSelected = value }
+
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        
+    }
+}
+
+@Composable
+fun CheckBoxes(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit
+) {
+    Row(
+        Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = imageSelected, onCheckedChange = onTitleClick )
+        Text("Image Title")
+        Spacer(modifier = Modifier.width(20.dp))
+        Checkbox(checked = linearSelected, onCheckedChange = onLinearClick )
+        Text("Linear Process")
+    }    
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PracticeTheme {
-        Greeting("Android")
-    }
+fun DemoPreview() {
+    CheckBoxes(linearSelected = true, imageSelected = true, onTitleClick = {}, onLinearClick = {})
 }
+    
+    
