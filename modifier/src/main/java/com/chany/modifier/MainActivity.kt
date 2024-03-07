54e658fff5384d0619a6c27adebed48abcc67373
chanydong <chanydong@kaonmedia.com>
2024-03-07 17:45:02 +0900

    preview setting

--- modifier/src/main/java/com/chany/modifier/MainActivity.kt
+++ modifier/src/main/java/com/chany/modifier/MainActivity.kt
@@ -3,13 +3,19 @@ package com.chany.modifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chany.modifier.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +28,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                 }
             }
         }
     }
+}
+
+@Composable
+fun DemoScreen(text: String) {
+    val modifier = Modifier
+        .border(width = 2.dp, color = Color.Black)
+        .padding(all =  10.dp)
+
+    Text(
+        text = text,
+        modifier,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DemoPreview() {
+    DemoScreen("sample")
 }
