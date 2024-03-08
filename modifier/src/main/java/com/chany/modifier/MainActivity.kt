d3d476786f36bfe02f121ea2e61e42206ac0f3ff
chanydong <chanydong@kaonmedia.com>
2024-03-08 17:31:16 +0900

    complete Modifier

--- modifier/src/main/java/com/chany/modifier/MainActivity.kt
+++ modifier/src/main/java/com/chany/modifier/MainActivity.kt
@@ -3,15 +3,25 @@ package com.chany.modifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,16 +49,40 @@ class MainActivity : ComponentActivity() {
 fun DemoScreen(text: String) {
     val modifier = Modifier
         .border(width = 2.dp, color = Color.Black)
-        .padding(all =  10.dp)
+        .padding(all = 10.dp)
 
+    Column (
+        Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "Hello Compose",
+            modifier = modifier,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        CustomImage(image = R.drawable.vacation,
+        Modifier.padding(16.dp).width(270.dp).clip(shape = RoundedCornerShape(30.dp)))
+
+    }
+    /*
     Text(
         text = text,
         modifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
+     */
 }
-
+@Composable
+fun CustomImage(image: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = null,
+        modifier)
+}
 @Preview(showBackground = true)
 @Composable
 fun DemoPreview() {
