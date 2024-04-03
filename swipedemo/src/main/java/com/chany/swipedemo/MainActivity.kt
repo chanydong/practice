c54a05f3a19d4e098cf73db971c904021b5d27b8
chanydong <chanydong@kaonmedia.com>
2024-04-03 13:28:01 +0900

    swipe func is stoped because of deprecated api

---
+++ swipedemo/src/main/java/com/chany/swipedemo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.chany.swipedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material.*
+import androidx.compose.material3.*
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chany.swipedemo.ui.theme.PracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun MainScreen() {
+    val parentBoxWidth = 320.dp
+    val childBoxSides = 30.dp
+
+
+
+
+
+}
+
